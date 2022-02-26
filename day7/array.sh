68f370eee9049dfff3666d968ca9149044ecd728
nilamkurhade <nilamkurhade87@gmail.com>
2022-02-26 16:21:42 +0530

    Added day7 programs

---
+++ day7/array.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -x
+declare -a fruits
+counter=0
+fruits[((counter++))]="Apple"
+fruits[((counter++))]="10"
+fruits[((counter++))]="Banana"
+
+echo ${fruits[@]}
+
+echo ${fruits[2]}
+
+echo ${#fruits[@]}
+
+echo ${!fruits[@]}
+
+unset fruits[1]
+echo ${fruits[@]}
+
+for i in ${!fruits[@]}
+do
+	echo ${fruits[$i]}
+done
